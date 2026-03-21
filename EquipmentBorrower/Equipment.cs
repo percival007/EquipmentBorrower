5951447c323839cc85146f705797fd7cb4150b05
whuzar <wojtekhuzar@gmail.com>
2026-03-21 23:55:49 +0100

    Add equipment models

---
+++ EquipmentBorrower/Equipment.cs
@@ -0,0 +1,10 @@
+﻿namespace EquipmentBorrower;
+
+public abstract class Equipment(string brand, float weight, string color)
+{
+
+    private string Brand { get; } = brand;
+    private float Weight { get; } = weight;
+    private string Color { get; } = color;
+    
+}
---
+++ EquipmentBorrower/Laptop.cs
@@ -0,0 +1,8 @@
+﻿namespace EquipmentBorrower;
+
+public class Laptop(string brand, float weight, string color, float screenResolution) : Equipment(brand, weight, color)
+{
+
+    private float ScreenResolution { get; } = screenResolution;
+    
+}
